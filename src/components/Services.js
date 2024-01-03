b11de4436c2dd60fca33447f2d66285363eeeb12
aimananjum <aimananjum2001@gmail.com>
2024-01-03 17:03:01 +0530

    initial commit

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,7 +1,81 @@
 import React from 'react';
+import {BsArrowUpRight} from 'react-icons/bs';
+import {motion} from 'framer-motion';
+import {fadeIn} from '../variants';
+
+const services = [
+  {
+    name:'Front-End Development',
+    description:
+      'It was popularised in the 1960s with the  and more recently with desktop.',
+    link:'learn more',
+  },
+  {
+    name:'Software Development',
+    description:
+      'It was popularised in the 1960s with the  and more recently with desktop.',
+    link:'learn more',
+  },
+  {
+    name:'Full stack Development',
+    description:
+      'It was popularised in the 1960s with the  and more recently with desktop.',
+    link:'learn more',
+  },
+  {
+    name:'Software Testing',
+    description:
+      'It was popularised in the 1960s with the  and more recently with desktop.',
+    link:'learn more',
+  },
+
+]
 
 const Services = () => {
-  return <div>Services</div>;
+  return <section id='services' className='section'>
+    <div className='container mx-auto'>
+      <div className='flex flex-col lg:flex-row'>
+        <motion.div className='flex-1 lg:bg-services lg:bg-bottom bg-no-repeat mix-blend-lighten mb-12 lg:mb-0'
+        variants={fadeIn('right',0.3)}
+        initial='hidden'
+        whileInView={'show'}
+        viewport={{once:false,amount:0.3}}>
+          <h2 className='h2 text-accent mb-6'>What I Do.</h2>
+          <h3 className='h3 max-w-[455px] mb-16'>I'm a Software developer at Hexaware Technologies.</h3>
+          <button className='btn btn-sm'>See my work</button>
+        </motion.div>
+        <motion.div className='flex-1'
+        variants={fadeIn('left',0.5)}
+        initial='hidden'
+        whileInView={'show'}
+        viewport={{once:false,amount:0.3}}>
+          <div>
+            {services.map((service,index)=>{
+              const {name,description,link}=service;
+              return (
+                <div className='border-b border-white/20 h-[120px] mb-[5px] flex' key={index}>
+                  <div className='max-w-[476px]'>
+                    <h4 className='text-[20px] tracking-wider font-primary font-semibold mb-4'>{name}</h4>
+                    <p className='font-secondary leading-tight'>{description}</p>
+                  </div>
+                  <div className='flex flex-col flex-1 items-end'>
+                    <a href='#'
+                    className='btn w-9 h-9 mb-[15px] flex justify-center items-center'
+                    >
+                      <BsArrowUpRight/>
+                    </a>
+                    <a href='#'
+                    className='text-gradient text-sm'
+                    >{link}</a>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </motion.div>
+      </div>
+    </div>
+  </section>;
 };
 
 export default Services;
--- src/components/Work.js
+++ src/components/Work.js
@@ -1,7 +1,85 @@
 import React from 'react';
+import {motion} from 'framer-motion';
+import {fadeIn} from '../variants';
 
+import Img1 from '../assets/portfolio-img1.png';
+import Img2 from '../assets/portfolio-img2.png';
+import Img3 from '../assets/portfolio-img3.png';
 const Work = () => {
-  return <div>Work</div>;
+  return <section id='work' className='section'>
+    <div className='container mx-auto'>
+      <div className='flex flex-col lg:flex-row gap-x-10'>
+        <motion.div
+        variants={fadeIn('right',0.5)}
+        initial='hidden'
+        whileInView={'show'}
+        viewport={{once:false,amount:0.3}}
+        className='flex-1 flex flex-col gap-y-12 mb-10 lg:mb-0'>
+          <div>
+            <h2 className='h2 leading-tight text-accent'>
+              My Latest<br/>
+              Work
+            </h2>
+            <p className='max-w-sm mb-16'>
+            It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop
+            </p>
+            <button className='btn btn-sm'>View all projects</button>
+          </div>
+          <div className='group relative overflow-hidden border-2 border-white/50 rounded-x1'>
+            <div className='group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300'></div>
+            <img className='group-hover:scale-125 transition-all duration-500'
+             src={Img1}
+             alt=''/>
+            <div
+            className='absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50'
+            ><span className='text-gradient'>Front-End Development</span>
+            </div>
+            <div className='absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50'>
+              <span className='text-3xl text-white'>Food Ordering App</span>
+            </div>
+          </div>
+        </motion.div>
+
+        <motion.div 
+        variants={fadeIn('left',0.3)}
+        initial='hidden'
+        whileInView={'show'}
+        viewport={{once:false,amount:0.3}}
+
+         className='flex-1 flex flex-col gap-y-10'>
+          <div className='group relative overflow-hidden border-2 border-white/50 rounded-x1'>
+            <div className='group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300'></div>
+            <img className='group-hover:scale-125 transition-all duration-500'
+             src={Img2}
+             alt=''/>
+            <div
+            className='absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50'
+            ><span className='text-gradient'>Front-End Development</span>
+            </div>
+            <div className='absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50'>
+              <span className='text-3xl text-white'>Food Ordering App</span>
+            </div>
+          </div>
+
+          <div className='group relative overflow-hidden border-2 border-white/50 rounded-x1'>
+            <div className='group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300' ></div>
+            <img className='group-hover:scale-125 transition-all duration-500'
+             src={Img3}
+             alt=''/>
+            <div
+            className='absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50'
+            ><span className='text-gradient'>Front-End Development</span>
+            </div>
+            <div className='absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50'>
+              <span className='text-3xl text-white'>Food Ordering App</span>
+            </div>
+          </div>
+          
+        </motion.div>
+        
+      </div>
+    </div>
+  </section>;
 };
 
 export default Work;
